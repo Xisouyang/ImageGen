e093ae1b3a4ebf00543c5e147177df0945eba7d5
Xi Ouyang <xisouyang@gmail.com>
2019-07-01 11:30:52 -0700

    first commit

---
+++ MemeGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeGen
+//
+//  Created by Stephen Ouyang on 7/1/19.
+//  Copyright © 2019 Stephen Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
